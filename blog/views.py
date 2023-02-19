489df535b10ad7edfb51bbd1d0662b261c4d0acb
Tomasz Mazur <tomimas231@gmail.com>
2023-02-19 14:58:50 +0100

    Created templates and static files for 'home' and 'about' pages

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# demo posts to test functionality
+posts = [
+    {
+        'author': 'Tomasz Mazur',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2022'
+    },
+    {
+        'author': 'Tadeusz Mazur',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2022'
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
