6840cdb6753755b697d4ba3b8239276e9b316594
Tomasz Mazur <tomimas231@gmail.com>
2023-02-21 14:12:31 +0100

    Created model Post and associated migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+# importing User model from admin
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # we delete Post with deleted author (User) of Post
+
+    # We define what information should be listed when calling object (Post)
+    def __str__(self) -> str:
+        return self.title
