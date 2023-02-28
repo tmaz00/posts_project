cf808313c4060b6bd39039723cb1403d50fe6bd1
Tomasz Mazur <tomimas231@gmail.com>
2023-02-28 21:35:10 +0100

    Created form and view for updating profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -14,3 +15,18 @@ class UserRegisterForm(UserCreationForm):
             'password1',    # names password1 and password2 are reserved
             'password2'
         ]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+        ]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
