44d8b00646b55c911671bc58e7adde8b087cefc4
Tomasz Mazur <tomimas231@gmail.com>
2023-02-28 22:04:18 +0100

    Added reduction of uploaded profile picture if is too big

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username} Profile"
+    
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)    # max size in pixels for image
+            img.thumbnail(output_size)  # we make thumbnail (pl: miniaturka) in given shape
+            img.save(self.image.path)
