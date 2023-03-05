f6935b61b0807d325b6eb0ce65249892e54c65f4
Tomasz Mazur <tomimas231@gmail.com>
2023-03-05 15:45:57 +0100

    Created List and Detail class-based posts views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,6 +8,16 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'   # set the name for model objects
+    ordering = ['-date_posted']      # '-' on the beginning means reversed direction of ordering
+
+class PostDetailView(DetailView):
+    model = Post
+    # default template name: blog/post-detail.html
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
